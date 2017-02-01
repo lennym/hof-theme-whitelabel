26aa2124da4296f31cb4b80141a29319799c9e2f
Leonard Martin <leonard.martin@gmail.com>
2017-02-01 15:48:18 +0000

    Add translations path

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const path = require('path');
 
 module.exports = {
-  views: path.resolve(__dirname, 'views')
+  views: path.resolve(__dirname, 'views'),
+  translations: path.resolve(__dirname, 'translations')
 };
