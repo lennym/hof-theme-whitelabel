ef5b1383762c81996745305642fba9d9add2a41b
Leonard Martin <leonard.martin@gmail.com>
2017-02-02 15:54:44 +0000

    Add basic bootstrap based theme

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const path = require('path');
+const partials = require('hof-template-partials');
+
+const partialsTranslations = path.join(path.dirname(require.resolve('hof-template-partials')), 'translations');
 
 module.exports = {
-  views: path.resolve(__dirname, 'views'),
-  translations: path.resolve(__dirname, 'translations')
+  views: [path.resolve(__dirname, 'views'), partials.views],
+  translations: [path.resolve(__dirname, 'translations'), partialsTranslations]
 };
