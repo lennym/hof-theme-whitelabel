69c9d203677aeb4620e187b0ea08963b14f905ce
Leonard Martin <leonard.martin@gmail.com>
2017-04-19 18:20:43 +0100

    Compile translation directories

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 const path = require('path');
 const partials = require('hof-template-partials');
-//const merge = require('lodash.merge');
+const merge = require('lodash').merge;
+const compile = require('./lib/compile');
 
 module.exports = () => {
   return (req, res, next) => {
     next();
-  }
-}
+  };
+};
 
-module.exports.views = [path.resolve(__dirname, 'views'), partials.views],
-module.exports.translations = partials.translations
+module.exports.views = [path.resolve(__dirname, 'views'), partials.views];
+module.exports.translations = merge(partials.resources(), compile(path.resolve(__dirname, 'translations/src')));
